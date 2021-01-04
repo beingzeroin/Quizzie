e3054421392bcb4b9a2aee99542950a2424fec1d
Zaki udding <zakiuddin321@gmail.com>
2021-01-04 23:12:36 +0530

    added all static pug pages in views folder

--- app.js
+++ app.js
@@ -6,18 +6,26 @@ const passportSetup = require("./Backend/api/config/passport-setup");
 const passportSetupAdmin = require("./Backend/api/config/passport-setup-admin");
 const passport = require("passport");
 const cors = require("cors");
-const cookieSession = require("cookie-session");
-const ipfilter = require('express-ipfilter').IpFilter
-const rateLimit = require("express-rate-limit");
-const schedule = require('node-schedule');
+
 const sgMail = require("@sendgrid/mail");
-const emailTemplates = require("./Backend/emails/email");
 
 sgMail.setApiKey(process.env.SendgridAPIKey);
 
-////routers
+//routers
 
 const app = express();
+var fs = require('fs');
+
+
+// var apiRouter = require('./Backend/api/routers/allRoutes');
+var async = require('async'),
+http = require('http');
+
+app.use(express.static('./Frontend/public'));
+app.set('port', process.env.PORT || 3000);
+
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 const userRoutes = require("./Backend/api/routers/user");
 const adminRoutes = require("./Backend/api/routers/admin");
@@ -51,28 +59,70 @@ mongoose.Promise = global.Promise;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-console.log(Date.now())
-// app.use(cookieSession({
-//     maxAge: 24 * 60 * 60 * 1000,
-//     keys: [keys.cookieSession]
-// }));
 
-// const ips = ['172.67.176.16','104.24.123.191','104.24.122.191','10.41.141.207','10.63.249.212','10.69.232.242','108.162.194.81','162.159.38.81','172.64.34.81','172.64.33.140','173.245.59.140','108.162.193.140','162.243.166.170','157.245.130.6']
-// // // Create the server
-// app.use(ipfilter(ips, { mode: 'allow' }))
+console.log(Date.now())
 
 // initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-/////Rate Limiter
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 150, // limit each IP to 100 requests per windowMs
-});
 
-//  apply to all requests
-app.use(limiter);
+app.get('/', async (req, res) => {
+	res.send('hi')
+  })
+
+function init() {
+	console.log("init");
+  app.get('/', (req, res) =>{
+	  console.log("rendering indx");
+	   res.render('index')
+	});
+	console.log('no')
+
+  // ALL SPECIFIC PAGES SHOULD BE CALLED HERE
+//   app.use('/api', apiRouter);
+//   app.use('/auth',authRouter);
+
+  // PLACEHOLDER FOR GETTING ANY PAGE FROM VIEWS
+  app.get('/:pagename', function (req, res) {
+    console.log("redirecting to");
+	console.log(req.params.pagename,__dirname);
+	// let pos = __dirname
+    if (fs.existsSync(__dirname + '/views/' + req.params.pagename + '.pug')) {
+      res.render(req.params.pagename, {
+        pageTitle: req.params.pagename
+      })
+    } else {
+      res.render('404', {
+        pageTitle: 'Page Not Found'
+      })
+    }
+  });
+
+  http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+  });
+}
+
+async.series([],
+	function (err, results) {
+	  if (err) {
+		console.log('Exception initializing database.');
+		throw err;
+	  } else {
+		console.log('Database initialization complete.');
+		init();
+	  }
+	});
+
+/////Rate Limiter
+// const limiter = rateLimit({
+// 	windowMs: 15 * 60 * 1000, // 15 minutes
+// 	max: 150, // limit each IP to 100 requests per windowMs
+// });
+
+// //  apply to all requests
+// app.use(limiter);
 
 // Allow CORS
 app.use((req, res, next) => {
@@ -90,18 +140,17 @@ app.use((req, res, next) => {
 
 app.use(cors());
 
-app.use("/user", userRoutes);
-app.use("/admin", adminRoutes);
-app.use("/quiz", quizRoutes);
-app.use("/question", questionRoutes);
-app.use("/auth", authRoutes);
-app.use("/general", generalRoutes);
-app.use("/owner", ownerRoutes);
-app.use("/auth/admin", authAdminRoutes);
+// app.use("/user", userRoutes);
+// app.use("/admin", adminRoutes);
+// app.use("/quiz", quizRoutes);
+// app.use("/question", questionRoutes);
+// app.use("/auth", authRoutes);
+// app.use("/general", generalRoutes);
+// app.use("/owner", ownerRoutes);
+// app.use("/auth/admin", authAdminRoutes);
 
-app.get('/', async (req, res) => {
-  res.send('hi')
-})
+
+
 
 //route not found
 app.use((req, res, next) => {
@@ -119,49 +168,46 @@ app.use((error, req, res, next) => {
 	});
 });
 
-////SEND REMINDER EMAIL 
-var rule = new schedule.RecurrenceRule();
+//SEND REMINDER EMAIL 
 
-rule.minute = new schedule.Range(0, 59, 5);
+// schedule.scheduleJob(rule, async function(){
+//   let currentTime = Date.now();
+//   const quizzes = await Quiz.find({
+//     reminderSent: {$ne: true},
+//     scheduledFor: {
+//       $lte: currentTime + 30*60*1000,
+//     }
+//   })
+//   console.log(`${quizzes.length} number of quizzes need to be reminded`);
 
-schedule.scheduleJob(rule, async function(){
-  let currentTime = Date.now();
-  const quizzes = await Quiz.find({
-    reminderSent: {$ne: true},
-    scheduledFor: {
-      $lte: currentTime + 30*60*1000,
-    }
-  })
-  console.log(`${quizzes.length} number of quizzes need to be reminded`);
-
-  for(let i = 0; i < quizzes.length; i++){
-    console.log(quizzes[i])
-    for(j = 0; j < quizzes[i].usersEnrolled.length; j++){
-      console.log(quizzes[i].usersEnrolled[j])
-      const user = await User.findById(quizzes[i].usersEnrolled[j].userId)
-      const msg = {
-        to: user.email,
-        from: process.env.sendgridEmail,
-        subject: "Quzzie: Quiz Reminder",
-        text: `This is an automatically genrated email sent from Quizzie. This is to remind you that your quiz ${quizzes[i].quizName} is scheduled at ${new Date(Number(quizzes[i].scheduledFor))}, Please login on time to not miss out on your quiz.`,
-      };
+//   for(let i = 0; i < quizzes.length; i++){
+//     console.log(quizzes[i])
+//     for(j = 0; j < quizzes[i].usersEnrolled.length; j++){
+//       console.log(quizzes[i].usersEnrolled[j])
+//       const user = await User.findById(quizzes[i].usersEnrolled[j].userId)
+//       const msg = {
+//         to: user.email,
+//         from: process.env.sendgridEmail,
+//         subject: "Quzzie: Quiz Reminder",
+//         text: `This is an automatically genrated email sent from Quizzie. This is to remind you that your quiz ${quizzes[i].quizName} is scheduled at ${new Date(Number(quizzes[i].scheduledFor))}, Please login on time to not miss out on your quiz.`,
+//       };
     
-      sgMail
-        .send(msg)
-        .then((result) => {
-          console.log(`Reminder email for ${quizzes[i].quizName} to ${msg.to}`)
-        })
-        .catch((err) => {
-          console.log('Some Error Occured', err.toString())
-        });
-    }  
-    await Quiz.updateOne({ _id: quizzes[i]._id }, { reminderSent: true })
-  }
-  // console.log(quizzes)
-});
+//       sgMail
+//         .send(msg)
+//         .then((result) => {
+//           console.log(`Reminder email for ${quizzes[i].quizName} to ${msg.to}`)
+//         })
+//         .catch((err) => {
+//           console.log('Some Error Occured', err.toString())
+//         });
+//     }  
+//     await Quiz.updateOne({ _id: quizzes[i]._id }, { reminderSent: true })
+//   }
+//   // console.log(quizzes)
+// });
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
-app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+// 	console.log(`Listening on port ${PORT}`);
+// });
